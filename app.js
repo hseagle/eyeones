dbe5ddc213f952f7522eb82d5cdb9c375ece1895
hseagle <hsxupeng@gmail.com>
2017-06-17 12:50:18 +0800

    use ES_SERVER_ADDR to specify the cluster addr

--- app.js
+++ app.js
@@ -10,13 +10,17 @@ var users = require('./routes/users');
 var db = require('./persistence/datastore')
 var clusterTbl = db.addCollection("cluster", { ttl: 20000, ttlInterval: 1000 })
 var eventTbl = db.addCollection("events", { ttl: 3600 * 1000, ttlInterval: 5000 })
-var indexTbl = db.addCollection("indices", {unique:'index', autoupdate:true})
-var shardTbl = db.addCollection("shards", {unique: 'uniq_id', autoupdate: true})
-var nodeTbl = db.addCollection("nodes",{unique:'name', autoupdate:true})
+var indexTbl = db.addCollection("indices", { unique: 'index', autoupdate: true })
+var shardTbl = db.addCollection("shards", { unique: 'uniq_id', autoupdate: true })
+var nodeTbl = db.addCollection("nodes", { unique: 'name', autoupdate: true })
 var rateTbl = db.addCollection("rates", { unique: 'metrics_name', autoupdate: true })
 var axios = require('axios')
 
 var app = express();
+
+var defaultEsServerAddr = "http://127.0.0.1:9200"
+
+var esServerAddr = process.env.ES_SERVER_ADDR || defaultEsServerAddr
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -53,7 +57,7 @@ app.use(function (err, req, res, next) {
 
 //获取集群的最新状态
 function tick() {
-    axios.get("http://10.8.122.215:9200/_cluster/stats?format=json").then(function (response) {
+    axios.get(`${esServerAddr}/_cluster/stats?format=json`).then(function (response) {
         var clusterInfo = response.data
         //check whether the state is changed
         var oldState = clusterTbl.get(clusterTbl.maxRecord("timestamp").index)
@@ -68,26 +72,25 @@ function tick() {
 
 //获取indices的最新状态
 function fetchIndicesInfo() {
-    var esServerAddr = "http://10.8.122.215:9200"
-    var indexFetchUrl = `${esServerAddr}/_cat/indices?bytes=b&h=index,status,health,docs.count,indexing.index_total,search.query_total,memory.total,pri,rep,fielddata.memory_size&format=json`
+    var indexFetchUrl = `${esServerAddr}/_cat/indices?bytes=mb&h=index,status,health,docs.count,indexing.index_total,search.query_total,memory.total,pri,rep,fielddata.memory_size,docs.count,store.size&format=json`
     axios.get(indexFetchUrl).then(function (response) {
         var indices = response.data
-        indices.forEach( function( item )  {
+        indices.forEach(function (item) {
             item['timestamp'] = (new Date()).getTime()
-            if ( item['status'] != 'open' || (item['index'].includes('.monitor') == true) ) return;
+            if (item['status'] != 'open' || (item['index'].includes('.monitor') == true)) return;
             var seriesCols = ['indexing.index_total', 'search.query_total', 'timestamp']
-            if ( indexTbl.count({index: item.index}) == 0 ) {
-                seriesCols.forEach( function(col) { item[col] = [item[col]] } )
+            if (indexTbl.count({ index: item.index }) == 0) {
+                seriesCols.forEach(function (col) { item[col] = [item[col]] })
                 indexTbl.insert(item)
-            }else{
-                var targetIndex = indexTbl.findOne({index: item.index})
-                
-                seriesCols.forEach( function(col) {
+            } else {
+                var targetIndex = indexTbl.findOne({ index: item.index })
+
+                seriesCols.forEach(function (col) {
                     targetIndex[col].push(item[col])
-                    if ( targetIndex[col].length > 20 ) targetIndex[col].shift()
+                    if (targetIndex[col].length > 20) targetIndex[col].shift()
                     delete item[col]
                 })
-               
+
                 Object.assign(targetIndex, item)
             }
         })
@@ -96,33 +99,32 @@ function fetchIndicesInfo() {
 
 //获取shards最新状态
 function fetchShardInfo() {
-    var esServerAddr = "http://10.8.122.215:9200"
     var shardFetchUrl = `${esServerAddr}/_cat/shards?h=index,shard,prirep,ip,node,indexing.index_total,search.query_total&format=json`
     axios.get(shardFetchUrl).then(function (response) {
         var shards = response.data
-        shards.forEach( function(shard) {
-            var uniqId = shard.index+'-'+shard.shard + '-' + shard.prirep;
-	    shard['timestamp'] = (new Date()).getTime()
-	    var seriesCol = ['indexing.index_total', 'search.query_total', 'timestamp']
-	    
-            if ( shardTbl.count({uniq_id: uniqId}) == 0 ) {
+        shards.forEach(function (shard) {
+            var uniqId = shard.index + '-' + shard.shard + '-' + shard.prirep;
+            shard['timestamp'] = (new Date()).getTime()
+            var seriesCol = ['indexing.index_total', 'search.query_total', 'timestamp']
+
+            if (shardTbl.count({ uniq_id: uniqId }) == 0) {
                 shard['uniq_id'] = uniqId
 
-		seriesCol.forEach(function(col) {
-		    shard[col] = [shard[col]]
-		})
+                seriesCol.forEach(function (col) {
+                    shard[col] = [shard[col]]
+                })
                 shardTbl.insert(shard)
-		
-            }else{
-                var targetShard = shardTbl.findOne({uniq_id: uniqId})
 
-		seriesCol.forEach(function(col) {
-		    targetShard[col].push(shard[col])
+            } else {
+                var targetShard = shardTbl.findOne({ uniq_id: uniqId })
 
-		    if ( targetShard[col].length > 20 ) targetShard[col].shift()
-		    delete shard[col]
-		})
-		
+                seriesCol.forEach(function (col) {
+                    targetShard[col].push(shard[col])
+
+                    if (targetShard[col].length > 20) targetShard[col].shift()
+                    delete shard[col]
+                })
+
                 Object.assign(targetShard, shard)
             }
         })
@@ -132,53 +134,52 @@ function fetchShardInfo() {
 
 //获取nodes最新状态
 function fetchNodeInfo() {
-    var esServerAddr = "http://10.8.122.215:9200"
-    var nodeFetchUrl = `${esServerAddr}/_cat/nodes?h=ip,name,load_5m,heap.percent,cpu,indexing.index_total,search.query_total,segments.count,node.role,master&format=json`
+    var nodeFetchUrl = `${esServerAddr}/_cat/nodes?h=ip,name,load_5m,disk.avail,heap.percent,cpu,indexing.index_total,search.query_total,segments.count,node.role,master&format=json`
     axios.get(nodeFetchUrl).then(function (response) {
         var nodes = response.data
-        
-        
-        nodes.forEach( function(item) {
-            var queryCon = {name: item.name}
-            var shardNum = shardTbl.find({node: item.name}).length
-            
+
+
+        nodes.forEach(function (item) {
+            var queryCon = { name: item.name }
+            var shardNum = shardTbl.find({ node: item.name }).length
+
             item['shard_num'] = shardNum
             item['timestamp'] = (new Date()).getTime()
-            
-            if ( nodeTbl.count( queryCon ) ==  0 ) {
-                var seriesCols = ['load_5m', 'heap.percent','cpu', 'indexing.index_total', 'search.query_total','timestamp']
-                
-                seriesCols.forEach(function(col){
+
+            if (nodeTbl.count(queryCon) == 0) {
+                var seriesCols = ['load_5m', 'heap.percent', 'cpu', 'indexing.index_total', 'search.query_total', 'timestamp']
+
+                seriesCols.forEach(function (col) {
                     item[col] = [item[col]]
                 })
-                
-                var rateCols = ['indexing', 'query']
-                rateCols.forEach( col=> item[col] = [])
-                
-                nodeTbl.insert(item)
-            }else{
-                var targetNode = nodeTbl.findOne(queryCon)
-                var seriesCols = ['load_5m', 'heap.percent','cpu', 'indexing.index_total', 'search.query_total','timestamp']
 
-                seriesCols.forEach( function(col) {
+                var rateCols = ['indexing', 'query']
+                rateCols.forEach(col => item[col] = [])
+
+                nodeTbl.insert(item)
+            } else {
+                var targetNode = nodeTbl.findOne(queryCon)
+                var seriesCols = ['load_5m', 'heap.percent', 'cpu', 'indexing.index_total', 'search.query_total', 'timestamp']
+
+                seriesCols.forEach(function (col) {
                     targetNode[col].push(item[col])
-                    if ( targetNode[col].length > 20 ) targetNode[col].shift()
+                    if (targetNode[col].length > 20) targetNode[col].shift()
                     delete item[col]
                 })
 
                 var rateColMeta = [
-                    {'seriesCol':'indexing.index_total', 'rateCol':'indexing'}, 
-                    {'seriesCol':'search.query_total', 'rateCol':'query'}
+                    { 'seriesCol': 'indexing.index_total', 'rateCol': 'indexing' },
+                    { 'seriesCol': 'search.query_total', 'rateCol': 'query' }
                 ]
 
-                var timeDiffs = targetNode['timestamp'].slice(1).map((val,idx) => {
+                var timeDiffs = targetNode['timestamp'].slice(1).map((val, idx) => {
                     return (val - targetNode['timestamp'][idx])
                 })
 
-                rateColMeta.map( function(item) {
+                rateColMeta.map(function (item) {
                     var series = targetNode[item.seriesCol]
-                    var rates = series.slice(1).map( function(val, idx) {
-                        return Math.ceil((val - series[idx])/(timeDiffs[idx]/1000))
+                    var rates = series.slice(1).map(function (val, idx) {
+                        return Math.ceil((val - series[idx]) / (timeDiffs[idx] / 1000))
                     })
                     targetNode[item.rateCol] = rates
                     //delete item[rateColMeta.seriesCol]
