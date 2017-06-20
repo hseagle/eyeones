3ea4907168314597aa22520a932dc82e2c640367
hseagle <hsxupeng@gmail.com>
2017-06-20 18:03:56 +0800

    add partial function to do the unit converting

--- client/src/EsNodeDetail.js
+++ client/src/EsNodeDetail.js
@@ -53,6 +53,17 @@ class EsNodeDetail extends Component {
         var nodeStats = this.state.nodestats
 
         var timeSeries = nodeStats.map(item => { return item.timestamp})
+        
+        const memoryConverter = (unit, sizeInBytes) => { 
+            var sizeMap = {
+                kb: {coefficient: 1024**1},
+                mb: {coefficient: 1024**2},
+                gb: {coefficient: 1024**3}
+            }
+
+            var rst = Math.ceil(Number(sizeInBytes) / Number(sizeMap[unit.toLowerCase()].coefficient)) + unit.toUpperCase()
+            return rst;
+        }
 
         //series data and rate data
         var metrics = [
@@ -64,9 +75,9 @@ class EsNodeDetail extends Component {
             { name: 'merge', path: 'indices.merges.total', series: [], isRate: true },
             { name: 'refresh', path: 'indices.refresh.total', series: [],isRate: true },
             { name: 'flush', path: 'indices.flush.total', series: [], isRate: true },
-            { name: 'fielddata', path: 'indices.fielddata.memory_size_in_bytes', series: [],isRate: false },
+            { name: 'fielddata', path: 'indices.fielddata.memory_size_in_bytes', series: [],isRate: false, convertFunc: memoryConverter.bind(null,'mb') },
             { name: 'segments', path: 'indices.segments.count', series: [], isRate:false },
-            { name: 'heap', path: 'jvm.mem.heap_used_in_bytes', series: [], isRate:false },
+            { name: 'heap', path: 'jvm.mem.heap_used_in_bytes', series: [], isRate:false, convertFunc: memoryConverter.bind(null,'gb') },
             { name: 'write_operations', path: 'fs.io_stats.total.write_operations', series: [], isRate: true },
             { name: 'read_operations', path: 'fs.io_stats.total.read_operations', series: [], isRate: true }
         ]
@@ -78,7 +89,9 @@ class EsNodeDetail extends Component {
             console.log(chartData)
             var series = metrics.forEach(metric => {
                 var targetPath = "item." + metric.path
-                metricJson[metric.name] = eval(targetPath)
+                var rawValue =  eval(targetPath)
+              
+                metricJson[metric.name] = rawValue
                 //caculating the rates
                 metricJson['hasRate'] = metric.isRate
                 if ( metric.isRate ) {
@@ -99,13 +112,20 @@ class EsNodeDetail extends Component {
         console.log(chartData)
         
         const dateFormat = (time) => { return moment(time).format('HH:mm:ss') }
-
-        var chartElements = metrics.filter( item=> item.isRate == true).map( metric => {
+        
+            
+        var rateCharts = metrics.filter( item=> item.isRate == true).map( metric => {
             var graphData = chartData.slice(1)
             var rateName = metric.name + "_rate"
-
+            const unitConvert = ( value ) => {
+                if ( metric.convertFunc != null )
+                return metric.convertFunc(value)
+                else
+                return value
+            }    
+            
             return (<div className="col-lg-6">
-                    <ResponsiveContainer width='95%' aspect={6.0/2.0}>
+                    <ResponsiveContainer width='95%' aspect={5.0/1.5}>
                     <AreaChart data={chartData.slice(1)}
                         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                         <defs>
@@ -119,11 +139,44 @@ class EsNodeDetail extends Component {
                             </linearGradient>
                         </defs>
                         <XAxis dataKey="timestamp" tickFormatter={dateFormat}/>
-                        <YAxis />
+                        <YAxis tickFormatter={unitConvert} />
                         <CartesianGrid strokeDasharray="3 3" />
                         <Tooltip />
-                        <Legend />
-                        <Area type="monotone" dataKey={rateName} stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                        <Legend verticalAlign="top"/>
+                        <Area type="monotone" dataKey={rateName} stroke="Lime" fillOpacity={1} fill="Coral" />
+                    </AreaChart>
+                    </ResponsiveContainer>
+                    </div>)
+        })
+
+        var histogramCharts = metrics.filter( item=> item.isRate == false ).map( metric => {
+            var graphData = chartData.slice(1)
+            const unitConvert = ( value ) => {
+                if ( metric.convertFunc != null )
+                return metric.convertFunc(value)
+                else
+                return value
+            }
+            return (<div className="col-lg-6">
+                    <ResponsiveContainer width='95%' aspect={5.0/1.5}>
+                    <AreaChart data={chartData}
+                        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey="timestamp" tickFormatter={dateFormat}/>
+                        <YAxis tickFormatter={unitConvert}/>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <Legend verticalAlign="top"/>
+                        <Area type="monotone" dataKey={metric.name} stroke="Lime" fillOpacity={1} fill="SeaGreen" />
                     </AreaChart>
                     </ResponsiveContainer>
                     </div>)
@@ -132,7 +185,8 @@ class EsNodeDetail extends Component {
         return (
             <div className="col-lg-11">
                 <div className="row">
-                    {chartElements}
+                    {histogramCharts}
+                    {rateCharts}
                 </div>
                 </div>
         )
