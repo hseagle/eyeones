dce5156b919fcf3403dd1258c99197556f7d155d
hseagle <hsxupeng@gmail.com>
2017-06-21 11:26:41 +0800

    remove unwanted log information

--- client/src/EsIndexDetail.js
+++ client/src/EsIndexDetail.js
@@ -40,9 +40,9 @@ class EsIndexDetail extends Component {
 		var fetchUrl = `/indexstats/${indexName}`
 		var self = this
 		axios.get(fetchUrl).then(response => {
-			console.log("receive response in tick()")
+
 			var jsonData = response.data
-			console.log(jsonData)
+
 			self.setState({ indexstats: jsonData })
 		}).catch(error => {
 			console.log(error)
@@ -68,142 +68,142 @@ class EsIndexDetail extends Component {
 
 		var indexStats = this.state.indexstats
 
-        var timeSeries = indexStats.map(item => { return item.timestamp})
-        
-        const memoryConverter = (unit, sizeInBytes) => { 
-            var sizeMap = {
-                kb: {coefficient: 1024**1},
-                mb: {coefficient: 1024**2},
-                gb: {coefficient: 1024**3}
-            }
+		var timeSeries = indexStats.map(item => { return item.timestamp })
 
-            var rst = Math.ceil(Number(sizeInBytes) / Number(sizeMap[unit.toLowerCase()].coefficient)) + unit.toUpperCase()
-            return rst;
-        }
+		const memoryConverter = (unit, sizeInBytes) => {
+			var sizeMap = {
+				kb: { coefficient: 1024 ** 1 },
+				mb: { coefficient: 1024 ** 2 },
+				gb: { coefficient: 1024 ** 3 }
+			}
 
-        //series data and rate data
-        var metrics = [
-            { name: 'timestamp', path: 'timestamp' },
-            { name: 'docs', path: 'primaries.docs.count', series: [], isRate: true },
-			{ name: 'store', path: 'primaries.store.size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null,'gb') },
-			{ name: 'fielddata', path: 'primaries.fielddata.memory_size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null,'mb') },
-			{ name: 'segments', path: 'primaries.segments.count', series: [], isRate: false},
-			{ name: 'segments.memory', path: 'primaries.segments.memory_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null,'mb')},
-			{ name: 'query_cache', path: 'primaries.query_cache.memory_size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null,'mb')},
-			{ name: 'search', path: 'primaries.search.query_total', series:[], isRate:true},
-			{ name: 'merge', path: 'primaries.merges.total', series:[], isRate:true},
-			{ name: 'refresh', path: 'primaries.refresh.total', series:[], isRate:true},
-			{ name: 'flush', path: 'primaries.flush.total', series:[], isRate:true},
-			{ name: 'indexing.throttle', path: 'primaries.indexing.throttle_time_in_millis', series:[], isRate:false}
-        ]
+			var rst = Math.ceil(Number(sizeInBytes) / Number(sizeMap[unit.toLowerCase()].coefficient)) + unit.toUpperCase()
+			return rst;
+		}
 
-        var chartData = []
-		
-        indexStats.forEach((item,idx) => {
-            var metricJson = {}
-            console.log("idx: " + idx)
-            console.log(chartData)
-            var series = metrics.forEach(metric => {
-                var targetPath = "item." + metric.path
-                var rawValue =  eval(targetPath)
-              
-                metricJson[metric.name] = rawValue
-                //caculating the rates
-                metricJson['hasRate'] = metric.isRate
-                if ( metric.isRate ) {
-                    if ( idx > 0 ) {
-                        var rawDiff = Number(metricJson[metric.name]) - Number(chartData[idx - 1][metric.name])
-                        var timeDiff = (Number(timeSeries[idx]) - Number(timeSeries[idx-1])) / 1000.0
-                        var rate = rawDiff / timeDiff
-                        console.log(`rawDiff ${rawDiff} timeDiff ${timeDiff}`)
-                        var rateName = metric.name + "_rate"
-                        metricJson[rateName] = Math.ceil(rate)
-                    }
-                }
-            })
-           
-            chartData.push(metricJson)
-        })
+		//series data and rate data
+		var metrics = [
+			{ name: 'timestamp', path: 'timestamp' },
+			{ name: 'docs', path: 'primaries.docs.count', series: [], isRate: true },
+			{ name: 'store', path: 'primaries.store.size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null, 'gb') },
+			{ name: 'fielddata', path: 'primaries.fielddata.memory_size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null, 'mb') },
+			{ name: 'segments', path: 'primaries.segments.count', series: [], isRate: false },
+			{ name: 'segments.memory', path: 'primaries.segments.memory_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null, 'mb') },
+			{ name: 'query_cache', path: 'primaries.query_cache.memory_size_in_bytes', series: [], isRate: false, convertFunc: memoryConverter.bind(null, 'mb') },
+			{ name: 'search', path: 'primaries.search.query_total', series: [], isRate: true },
+			{ name: 'merge', path: 'primaries.merges.total', series: [], isRate: true },
+			{ name: 'refresh', path: 'primaries.refresh.total', series: [], isRate: true },
+			{ name: 'flush', path: 'primaries.flush.total', series: [], isRate: true },
+			{ name: 'indexing.throttle', path: 'primaries.indexing.throttle_time_in_millis', series: [], isRate: false }
+		]
+
+		var chartData = []
+
+		indexStats.forEach((item, idx) => {
+			var metricJson = {}
+			
+			
+			var series = metrics.forEach(metric => {
+				var targetPath = "item." + metric.path
+				var rawValue = eval(targetPath)
+
+				metricJson[metric.name] = rawValue
+				//caculating the rates
+				metricJson['hasRate'] = metric.isRate
+				if (metric.isRate) {
+					if (idx > 0) {
+						var rawDiff = Number(metricJson[metric.name]) - Number(chartData[idx - 1][metric.name])
+						var timeDiff = (Number(timeSeries[idx]) - Number(timeSeries[idx - 1])) / 1000.0
+						var rate = rawDiff / timeDiff
+						
+						var rateName = metric.name + "_rate"
+						metricJson[rateName] = Math.ceil(rate)
+					}
+				}
+			})
+
+			chartData.push(metricJson)
+		})
 
 		const dateFormat = (time) => { return moment(time).format('HH:mm:ss') }
-		
-var rateCharts = metrics.filter( item=> item.isRate == true).map( metric => {
-            var graphData = chartData.slice(1)
-            var rateName = metric.name + "_rate"
-            const unitConvert = ( value ) => {
-                if ( metric.convertFunc != null )
-                return metric.convertFunc(value)
-                else
-                return value
-            }    
-            
-            return (<div className="col-lg-6">
-                    <ResponsiveContainer width='100%' aspect={6.0/1.5}>
-                    <LineChart data={chartData.slice(1)}
-                        margin={{ top: 10, right: 20, left: 10, bottom: 0 }}>
-                        <XAxis dataKey="timestamp" tickFormatter={dateFormat}/>
-                        <YAxis tickFormatter={unitConvert} />
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Tooltip />
-                        <Legend verticalAlign="top"/>
-                        <Line type="monotone" dataKey={rateName} stroke="#82ca9d" fillOpacity={1} fill="Coral" isAnimationActive={false} />
-                    </LineChart>
-                    </ResponsiveContainer>
-                    </div>)
-        })
+
+		var rateCharts = metrics.filter(item => item.isRate == true).map(metric => {
+			var graphData = chartData.slice(1)
+			var rateName = metric.name + "_rate"
+			const unitConvert = (value) => {
+				if (metric.convertFunc != null)
+					return metric.convertFunc(value)
+				else
+					return value
+			}
+
+			return (<div className="col-lg-6">
+				<ResponsiveContainer width='100%' aspect={6.0 / 1.5}>
+					<LineChart data={chartData.slice(1)}
+						margin={{ top: 10, right: 20, left: 10, bottom: 0 }}>
+						<XAxis dataKey="timestamp" tickFormatter={dateFormat} />
+						<YAxis tickFormatter={unitConvert} />
+						<CartesianGrid strokeDasharray="3 3" />
+						<Tooltip />
+						<Legend verticalAlign="top" />
+						<Line type="monotone" dataKey={rateName} stroke="#82ca9d" fillOpacity={1} fill="Coral" isAnimationActive={false} />
+					</LineChart>
+				</ResponsiveContainer>
+			</div>)
+		})
 
 
-		var histogramCharts = metrics.filter( item=> item.isRate == false ).map( metric => {
-            var graphData = chartData.slice(1)
-            const unitConvert = ( value ) => {
-                if ( metric.convertFunc != null )
-                return metric.convertFunc(value)
-                else
-                return value
-            }
-            return (<div className="col-lg-6">
-                    <ResponsiveContainer width='100%' aspect={6.0/1.5}>
-                    <AreaChart data={chartData}
-                        margin={{ top: 10, right: 20, left: 10, bottom: 0 }}>
-                        <defs>
-                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                            </linearGradient>
-                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-                                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-                            </linearGradient>
-                        </defs>
-                        <XAxis dataKey="timestamp" tickFormatter={dateFormat}/>
-                        <YAxis tickFormatter={unitConvert}/>
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <Tooltip />
-                        <Legend verticalAlign="top"/>
-                        <Area type="monotone" dataKey={metric.name} stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
-                    </AreaChart>
-                    </ResponsiveContainer>
-                    </div>)
-        })
+		var histogramCharts = metrics.filter(item => item.isRate == false).map(metric => {
+			var graphData = chartData.slice(1)
+			const unitConvert = (value) => {
+				if (metric.convertFunc != null)
+					return metric.convertFunc(value)
+				else
+					return value
+			}
+			return (<div className="col-lg-6">
+				<ResponsiveContainer width='100%' aspect={6.0 / 1.5}>
+					<AreaChart data={chartData}
+						margin={{ top: 10, right: 20, left: 10, bottom: 0 }}>
+						<defs>
+							<linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+								<stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+							</linearGradient>
+							<linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+								<stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+								<stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+							</linearGradient>
+						</defs>
+						<XAxis dataKey="timestamp" tickFormatter={dateFormat} />
+						<YAxis tickFormatter={unitConvert} />
+						<CartesianGrid strokeDasharray="3 3" />
+						<Tooltip />
+						<Legend verticalAlign="top" />
+						<Area type="monotone" dataKey={metric.name} stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+					</AreaChart>
+				</ResponsiveContainer>
+			</div>)
+		})
 
-              
+
 		var choosedIndex = this.state.indexname
 
 
 		return (
-            <div className="col-lg-11">
-                <div className="row">
-                        <ol className="breadcrumb">
-                            <li><Link to="/indices">indices</Link></li>
-                            <li className="active">{this.state.indexname}</li>
-                        </ol>
-                    </div>
-                <div className="row">
+			<div className="col-lg-11">
+				<div className="row">
+					<ol className="breadcrumb">
+						<li><Link to="/indices">indices</Link></li>
+						<li className="active">{this.state.indexname}</li>
+					</ol>
+				</div>
+				<div className="row">
 					{rateCharts}
-                    {histogramCharts}
-                </div>
-                </div>
-        )
+					{histogramCharts}
+				</div>
+			</div>
+		)
 	}
 }
 
--- routes/index.js
+++ routes/index.js
@@ -2,72 +2,70 @@ var express = require('express');
 var router = express.Router();
 var db = require('../persistence/datastore')
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   var cnt = db.getCollection('cluster').count();
   res.render('index', { title: cnt });
 });
 
-router.get('/cluster/stats', function(req, res, next) {
+router.get('/cluster/stats', function (req, res, next) {
   var cluster = db.getCollection('cluster')
-  var clusterInfo =  cluster.get(cluster.maxRecord("timestamp").index)
-  if ( clusterInfo != null ) {
+  var clusterInfo = cluster.get(cluster.maxRecord("timestamp").index)
+  if (clusterInfo != null) {
     clusterInfo['timestamp'] = (new Date()).toLocaleString()
     res.send(clusterInfo)
   }
 })
 
-router.get('/cluster/history', function(req, res, next) {
+router.get('/cluster/history', function (req, res, next) {
   var cluster = db.getCollection('cluster')
-  var jsonData = {"total": cluster.count()}
+  var jsonData = { "total": cluster.count() }
   res.send(jsonData)
 })
 
-router.get('/indices', function(req, res, next) {
+router.get('/indices', function (req, res, next) {
   var index = db.getCollection('indices')
   var jsonData = index.chain().find().data()
   res.send(jsonData)
 })
 
-router.get('/shards', function(req, res, next) {
+router.get('/shards', function (req, res, next) {
   var shards = db.getCollection('shards')
   var jsonData = shards.find()
   res.send(jsonData)
 })
 
-router.get('/nodes', function(req, res, next) {
+router.get('/nodes', function (req, res, next) {
   var nodes = db.getCollection('nodes')
   var jsonData = nodes.find()
   res.send(jsonData)
 })
 
-router.get('/nodestats', function(req,res,next) {
+router.get('/nodestats', function (req, res, next) {
   var nodeStats = db.getCollection('nodeStats')
   var jsonData = nodeStats.chain().find().limit(1).data()
   res.send(jsonData)
 })
 
-router.get('/nodestats/:nodename', function(req,res, next) {
+router.get('/nodestats/:nodename', function (req, res, next) {
   console.log(req.params)
   var nodeName = req.params.nodename
   var nodeStatsTbl = db.getCollection('nodeStats')
-  var matchedNodes = nodeStatsTbl.chain().find({name: nodeName}).simplesort('timestamp').data()
+  var matchedNodes = nodeStatsTbl.chain().find({ name: nodeName }).simplesort('timestamp').data()
   res.send(matchedNodes)
 })
 
-router.get('/indexstats', function(req, res, next) {
-    var indexStatsTbl = db.getCollection('indexStats')
-    var jsonData = indexStatsTbl.chain().find().limit(1).data()
-    res.send(jsonData)
+router.get('/indexstats', function (req, res, next) {
+  var indexStatsTbl = db.getCollection('indexStats')
+  var jsonData = indexStatsTbl.chain().find().limit(1).data()
+  res.send(jsonData)
 })
 
-router.get('/indexstats/:indexname', function(req,res, next) {
-  console.log(req.params)
+router.get('/indexstats/:indexname', function (req, res, next) {
   var indexName = req.params.indexname
   var indexStatsTbl = db.getCollection('indexStats')
-    var matchedIndices = indexStatsTbl.chain().find({indexname: indexName}).simplesort('timestamp').data()
-    console.log(matchedIndices)
+  var matchedIndices = indexStatsTbl.chain().find({ indexname: indexName }).simplesort('timestamp').data()
   res.send(matchedIndices)
 })
-    
+
 
 module.exports = router;
